ed36e3eefe8caf9d6132167c3273573a628bc753
Paul-blgrn <paul.blgrn@gmail.com>
2024-10-27 19:32:48 +0100

    Updates

--- src/store/Store.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from "redux";
-import { cartReducer } from "../reducers/cookieClicker/CookieClickerReducer";
-
-const store = createStore(cartReducer);
-
-export default store;
